9154427a0ceb8f55ccd79fde7151ab9ed5468bd6
skykholodov <skykholodov@gmail.com>
2026-04-25 04:55:31 +0500

    Added vite.config.ts

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { TabPanel } from "./TabPanel/TabPanel";
+export type { TabPanelItem } from "./TabPanel/TabPanel";
+export { PaginatedList } from "./PaginatedList/PaginatedList";
+export type { PaginatedListItem } from "./PaginatedList/PaginatedList";
+export { Combobox } from "./Combobox/Combobox";
+export type { ComboboxOption } from "./Combobox/Combobox";
+export { SkipLink } from "./SkipLink/SkipLink";
+export { useReducedMotion, useFocusTrap, useLiveRegion, useHighContrast } from "./SkipLink/SkipLink";
